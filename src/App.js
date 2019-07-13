b460346befced10cc71bd2f78496076b49bc3f35
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-13 16:27:30 +0200

    add create project form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
-import SignIn from './components/auth/SignIn'
-import SignUp from './components/auth/SignUp'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
