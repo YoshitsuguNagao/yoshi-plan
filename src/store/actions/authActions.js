69610e04448a315714a43c4e635d19e83476fe1c
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-16 19:54:58 +0200

    add signup

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,3 +1,5 @@
+import { firestore } from "firebase";
+
 export const signIn = (credentials) => {
   return (dispatch, getState, {getFirebase}) => {
     const firebase = getFirebase();
@@ -22,6 +24,29 @@ export const signOut = () => {
     }).catch((err) => {
       dispatch({ type: 'SIGNOUT_ERROR', err })
 
+    })
+  }
+}
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    firebase.auth().createUserWithEmailAndPassword(
+      newUser.email,
+      newUser.password,
+    )
+    .then((res) => {
+      return firestore.collection('user').doc(res.user.uid).set({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0] + newUser.lastName[0],
+      })
+    })
+    .then(() => {
+      dispatch({ type: 'SIGNUP_SUCCESS' })
+    }).catch((err) => {
+      dispatch({ type: 'SIGNUP_ERROR', err })
     })
   }
 }
