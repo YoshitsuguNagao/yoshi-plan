04284b7db3751981edc93094c6cc651e52066fd1
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-17 10:53:21 +0200

    done the course

---
+++ dist/precache-manifest.759663d21e514707dbfcb509417b5bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acf404cf79e48c1e0ad76d1656c6be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e367ebe9605ccfe289c8",
+    "url": "/static/css/main.8237fe15.chunk.css"
+  },
+  {
+    "revision": "f53d73bdb3ee2a458823",
+    "url": "/static/js/2.aa10ea48.chunk.js"
+  },
+  {
+    "revision": "e367ebe9605ccfe289c8",
+    "url": "/static/js/main.6356dd5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
